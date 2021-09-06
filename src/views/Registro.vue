67dd9b515053ac97eb734853369d4ea3c3bda3d6
Italo <italoramirezp@gmail.com>
2021-09-06 17:32:15 -0500

    Formulario Registro con View registro

---
+++ src/views/Registro.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <h1 class="my-5">Registro de Usuario</h1>
+        <form>
+            <input 
+                type="email" 
+                placeholder="Email"
+                class="form-control my-2"
+            >
+            <input 
+                type="password" 
+                placeholder="password"
+                class="form-control my-2"
+            >
+            <input 
+                type="password" 
+                placeholder="password"
+                class="form-control my-2"
+            >
+
+            <button 
+                type="submit"
+                class="btn btn-primary"
+            >
+                Registrar
+            </button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
